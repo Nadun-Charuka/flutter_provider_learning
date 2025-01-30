e9ecedbd884559f8f30cbd259c2e64d2f7b09115
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-01-30 11:54:17 +0530

    basic done

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7];
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            int last = numbers.last;
+            setState(() {
+              numbers.add(last + 1);
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+        appBar: AppBar(),
+        body: SizedBox(
+          child: Column(
+            children: [
+              Text(
+                numbers.last.toString(),
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: numbers.length,
+                  itemBuilder: (context, index) {
+                    return Text(
+                      numbers[index].toString(),
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
